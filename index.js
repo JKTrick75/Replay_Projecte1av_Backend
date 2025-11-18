a2d0b92b39f9023d87f86206fee24bae7bb1d8e9
JKTrick75 <david.borde75@gmail.com>
2025-11-18 12:27:34 +0100

    Preparem backend despliegue aws

--- index.js
+++ index.js
@@ -2,14 +2,31 @@
 const express = require('express'); 
 const mongoose = require('mongoose'); 
 const cors = require('cors'); //Cors para permitir el fetch desde externos
+const fs = require('fs'); //(File System)
+const path = require('path');//(path)
 
 //RUTAS
 const consolaRouter = require('./routes/consola');
 const juegoRouter = require('./routes/juego');
 const marcaRouter = require('./routes/marca');
 
-//CONEXIÓN MONGODB
-mongoose.connect('mongodb://127.0.0.1:27017/replay'); 
+// //CONEXIÓN MONGODB
+// mongoose.connect('mongodb://127.0.0.1:27017/replay');
+
+// --- CONEXIÓN A DOCUMENTDB ---
+
+// 3. Define la ruta al certificado (que está en la misma carpeta)
+const ca = path.join(__dirname, 'global-bundle.pem');
+
+// 4. Pega tu URL (con la contraseña y BBDD añadidas)
+const MONGO_URI = 'mongodb://replayadmin:Patata1?@replay.cluster-c5xld3ueaqnc.us-east-1.docdb.amazonaws.com:27017/replay?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
+
+// 5. Conecta usando las opciones de SSL
+mongoose.connect(MONGO_URI, {
+  tls: true,
+  tlsCAFile: ca // Pásale el certificado
+});
+// -----------------------------
 
 // ================================================================================================================== //
 
