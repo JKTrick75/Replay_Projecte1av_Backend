6f2f8a529893e72fd3b49c19c636fa0077a0941e
JKTrick75 <david.borde75@gmail.com>
2025-11-18 23:40:45 +0100

    connectar amb mongo atlas (núvol)

--- index.js
+++ index.js
@@ -2,31 +2,28 @@
 const express = require('express'); 
 const mongoose = require('mongoose'); 
 const cors = require('cors'); //Cors para permitir el fetch desde externos
-const fs = require('fs'); //(File System)
-const path = require('path');//(path)
 
 //RUTAS
 const consolaRouter = require('./routes/consola');
 const juegoRouter = require('./routes/juego');
 const marcaRouter = require('./routes/marca');
 
-// //CONEXIÓN MONGODB
+// ========================================================================
+// CONEXIÓN MONGODB
+// ========================================================================
+
 // mongoose.connect('mongodb://127.0.0.1:27017/replay');
 
-// --- CONEXIÓN A DOCUMENTDB ---
+// ========================================================================
+// CONEXIÓN MONGODB ATLAS
+// ========================================================================
 
-// 3. Define la ruta al certificado (que está en la misma carpeta)
-const ca = path.join(__dirname, 'global-bundle.pem');
+const MONGO_URI = 'mongodb+srv://DavidMB:Patata123@cluster0.dmsjo4l.mongodb.net/Replay?appName=Cluster0';
 
-// 4. Pega tu URL (con la contraseña y BBDD añadidas)
-const MONGO_URI = 'mongodb://replayadmin:Patata1?@replay.cluster-c5xld3ueaqnc.us-east-1.docdb.amazonaws.com:27017/replay?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Conectado a MongoDB Atlas'))
+  .catch((err) => console.error('Error conectando a Atlas:', err));
 
-// 5. Conecta usando las opciones de SSL
-mongoose.connect(MONGO_URI, {
-  tls: true,
-  tlsCAFile: ca // Pásale el certificado
-});
-// -----------------------------
 
 // ================================================================================================================== //
 
@@ -47,7 +44,6 @@ app.use(express.urlencoded());  //Para métodos tradicionales de envío de datos
 app.use('/consola', consolaRouter);
 app.use('/juego', juegoRouter);
 app.use('/marca', marcaRouter);
-
 // ================================================================================================================== //
 
 //LANZAMOS EXPRESS (última línea)
